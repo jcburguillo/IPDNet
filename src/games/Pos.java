597f4532c63ecd168a9643dc30ce2ef11de69c00
Juan C. Burguillo <30867677+jcburguillo@users.noreply.github.com>
2022-05-04 19:44:10 +0200

    Add files via upload

---
+++ src/games/Pos.java
@@ -0,0 +1,20 @@
+package games;
+
+/**
+	* Esta clase se usa para meter en un Vector posiciones de la matriz.
+	*
+	* @author  Juan C. Burguillo Rial
+	* @version 1.0
+	*/
+public class Pos
+{
+public int x;
+public int y;
+
+public Pos (int xAux, int yAux) {
+	x = xAux;
+	y = yAux;
+	}
+
+}
+
